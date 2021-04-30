5ee7b6cbf5979b4f4fccf906cdefee31e798e0f4
vyaskhushboo26 <vyas.khushboo26@gmail.com>
2021-04-30 16:57:08 +0530

    Decimal value checked and clear display

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-//Get the existing value in 'cname' element
-
-function multiply() {}
-
-function digitPressed1() {
-  updateDisplayWithValue("1");
-}
-
-function digitPressed2() {
-  updateDisplayWithValue("2");
-}
-
 function updateDisplayWithValue(digit) {
   //Get the existing value in 'cname' element
   var existingValue = document.getElementById("cname").value;
-  console.log('existingValue:', existingValue);
+  console.log("existingValue:", existingValue);
   //if value is zero
   if (existingValue == "0") {
     //show digit in display
     document.getElementById("cname").value = digit;
-  } else {
-    //else not zero
-    //append the digit in existing value and display
-    var newValue = digit + existingValue ;
-    document.getElementById("cname").value = newValue;
+    return;
   }
+  // if digit is desimal point then check if it alerady exist(54.6)
+  if (digit == ".") {
+    let index = existingValue.indexOf(".");
+    if (index >= 0) {
+      // if yes then do nothing
+      return;
+    }
+  }
+
+  //else not zero
+  //append the digit in existing value and display
+  var newValue = existingValue + digit;
+  document.getElementById("cname").value = newValue;
+}
+
+function myClearContainer() {
+  document.getElementById("cname").value = "0";
 }
